24f0620dd5ceb86a788a8edc9096613026b4d2fb
majid-757 <majid.asadollahi95@gmail.com>
2022-04-08 23:58:08 +0430

    abstract_factory

---
+++ abstract factory/abstract_factory.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+
+
+class Car(ABC):
+    @abstractmethod
+    def call_suv(self):
+        pass
+
+
+    @abstractmethod
+    def call_coupe(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
