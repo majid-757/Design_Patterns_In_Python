fd6e33df469b452d66bf34fb1f5c7e9ae85688d4
majid-757 <majid.asadollahi95@gmail.com>
2022-04-13 00:14:08 +0430

    builder

--- builder/builder.py
+++ builder/builder.py
@@ -19,6 +19,39 @@ class Benz(Builder):
         return body
 
 
+    def getEngine(self):
+        engine = Engine()
+        engine.hp = 500
+        return engine
+
+
+    def getWheel(self):
+        wheel = Wheel()
+        wheel.size = 22
+        return wheel
+
+
+class Bmw(Builder):
+    def getBody(self):
+        body = Body()
+        body.shape = 'Sedan'
+        return body
+
+
+    def getEngine(self):
+        engine = Engine()
+        engine.hp = 340
+        return engine
+
+
+    def getWheel(self):
+        wheel = Wheel()
+        wheel.size = 18
+        return wheel
+
+
+
+
 
 class Wheel:
     size = None
