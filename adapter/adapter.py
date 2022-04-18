4f676481f105b0ee4c374bf07aae4183f4576df6
majid-757 <majid.asadollahi95@gmail.com>
2022-04-19 00:09:30 +0430

    adapter

--- adapter/adapter.py
+++ adapter/adapter.py
@@ -11,14 +11,27 @@ class Adapter:
         self._socket = socket
 
 class Fridge:
+    _adapter = None
     _pinType = '3'
 
+    def __init__(self, adapter):
+        self._adapter = adapter
+
+
+    def freeze(self):
+        if self._adapter._pinType == (self._pinType + 'To' + self._adapter._socket._type):
+
+            print('Done...')
+
+        else:
+
+            print('Sorry , Not Usable')
 
 
 
+i = IranSocket()
+adapter = Adapter(i)
 
+f = Fridge(adapter)
 
-
-
-
-
+f.freeze()
