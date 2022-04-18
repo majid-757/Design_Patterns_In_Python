62437e162c0ab4f69c8cb3668ec4fe369265bd07
majid-757 <majid.asadollahi95@gmail.com>
2022-04-18 23:57:00 +0430

    adapter

---
+++ adapter/adapter.py
@@ -0,0 +1,24 @@
+
+class IranSocket:
+    _type = '2'
+
+
+class Adapter:
+    _socket = None
+    _pinType = '3To2'
+
+    def __init__(self, socket):
+        self._socket = socket
+
+class Fridge:
+    _pinType = '3'
+
+
+
+
+
+
+
+
+
+
