20afc06b049c2710f16baeeb9fe0f2cd9426e26c
majid-757 <majid.asadollahi95@gmail.com>
2022-04-24 00:21:30 +0430

    facade

--- facade/facade.py
+++ facade/facade.py
@@ -5,8 +5,54 @@ class Raw:
 
 
 
+class Transfer:
+    def transfer(self):
+        print("Transfering raw foods to restaurant...")
 
 
-class ItalianRestaurant
+
+class Cook:
+    def cook(self):
+        print('Cooking raw foods by chief...')
+
+
+
+class Serve:
+    def serve(self):
+        print('Serving food to client')
+
+
+
+
+class ItalianRestaurant:
+    def get(self):
+        r = Raw()
+        r.raw()
+
+
+        t = Transfer()
+        t.transfer()
+
+
+        c = Cook()
+        c.cook()
+
+
+        s = Serve()
+        s.serve()
+
+
+ 
+
+def order():
+    i = ItalianRestaurant()
+    i.get()
+
+
+
+
+order()
+
+
 
 
